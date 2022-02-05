1be0bc1e85c9a774d20d6595930544cca1d5748a
Tomas Kaminskas <kaminskas.tomas@gmail.com>
2022-02-05 15:14:44 +0000

    files save/delete

--- take_measurements.py
+++ take_measurements.py
@@ -2,4 +2,10 @@ import cv2
 import numpy as np
 import os
 
-os.system('fswebcam -r 320x240 -S 3 --jpeg 50 --save /home/pi/%H%M%S.jpg')
+folder = '/home/pi/images/'
+os.system('fswebcam -r 640x480 -S 3 --jpeg 50 --save /home/pi/images/%H%M%S.jpg')
+for filename in os.listdir(folder):
+	img = cv2.imread(os.path.join(folder,filename))
+	if img is not None:
+		gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+		os.remove(os.path.join(folder,filename))
